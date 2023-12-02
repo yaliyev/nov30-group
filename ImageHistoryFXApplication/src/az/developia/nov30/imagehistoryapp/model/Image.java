e342a2574934b7837e1d52e1cfe1f4c2591c3a2f
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-12-02 21:04:44 +0400

    Image model created

---
+++ ImageHistoryFXApplication/src/az/developia/nov30/imagehistoryapp/model/Image.java
@@ -0,0 +1,56 @@
+package az.developia.nov30.imagehistoryapp.model;
+
+import java.time.LocalDate;
+
+public class Image {
+	private Integer id;
+	private String path;
+	private LocalDate insertDate;
+	private Integer timesUsed;
+
+	// CTRL + SHIFT + F Code Format
+
+	public Image() {
+		super();
+	}
+
+	public Image(String path, LocalDate insertDate, Integer timesUsed) {
+		super();
+		this.path = path;
+		this.insertDate = insertDate;
+		this.timesUsed = timesUsed;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public LocalDate getInsertDate() {
+		return insertDate;
+	}
+
+	public void setInsertDate(LocalDate insertDate) {
+		this.insertDate = insertDate;
+	}
+
+	public Integer getTimesUsed() {
+		return timesUsed;
+	}
+
+	public void setTimesUsed(Integer timesUsed) {
+		this.timesUsed = timesUsed;
+	}
+
+}
