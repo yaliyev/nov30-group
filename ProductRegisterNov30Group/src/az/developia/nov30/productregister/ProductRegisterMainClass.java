7b3a23c831aa887b64b582842aeadbcac9b5dddc
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2024-01-06 21:01:50 +0400

    design part completed

--- ProductRegisterNov30Group/src/az/developia/nov30/productregister/ProductRegisterMainClass.java
+++ ProductRegisterNov30Group/src/az/developia/nov30/productregister/ProductRegisterMainClass.java
@@ -1,5 +1,32 @@
 package az.developia.nov30.productregister;
 
-public class ProductRegisterMainClass {
+import java.io.IOException;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ProductRegisterMainClass extends Application{
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage)  {
+		try {
+			AnchorPane pane  = FXMLLoader.load(getClass().getClassLoader().getResource("az/developia/nov30/productregister/view/product.fxml"));
+			Scene scene = new Scene(pane);
+			stage.setScene(scene);
+			stage.setTitle("Product Register App Nov30 Group");
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	
 }
